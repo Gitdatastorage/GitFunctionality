6cb1c4fc6b64ac2828aa67041d15c29a100243fc
Akash <hardworkers@gmail.com>
2018-07-27 15:14:26 +0530

    Adding

---
+++ GitFunctionality/src/AddingNewFile.java
@@ -0,0 +1,8 @@
+
+public class AddingNewFile {
+
+	public static void main(String[] args) {
+		System.out.println("Adding and Jenkins");
+		System.out.println("testing");
+	}
+}
---
+++ GitFunctionality/src/PullingFromGit.java
@@ -0,0 +1,7 @@
+
+public class PullingFromGit {
+
+	public static void main(String[] args) {
+		System.out.println("Pushing");
+	}
+}
